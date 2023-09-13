2f2c9f5b181f6ed03b02e9a907d55377859b61a4
AlbertSMEETS <albert.smeets@laposte.net>
2023-09-13 11:28:30 +0200

    New variable names + Optimisation
    Changing variables names to add new variables, getting all lines in the while and adding some print to get the code clean.

--- main.py
+++ main.py
@@ -2,38 +2,48 @@ import random
 import string
 
 play = True
+tries = 0
+maxTries = 12
 
 X = 0  #right color, right place
 Y = 0  #right color, wrong place
 
 colors = ['R','G','B','Y','P','W']
 code = random.sample(colors,4)
+code_test = code.copy()
 print(code)
 
-tries = []
-        #to try and guess 5 colors, just add a fifth.
-first_l = input("Write your first letter :  ")
-tries.append(first_l)
-             
-second_l = input("Write your second letter :  ")
-tries.append(second_l)
-             
-third_l = input("Write your third letter :  ")
-tries.append(third_l)
-             
-fourth_l = input("Write your fourth letter :  ")
-tries.append(fourth_l)
-
-print(tries)
 
 while (play):
-    for i in range(len(code)):
-        if tries[i] == code[i]:
-            X +=1
-    for i in range(len(code)):
-        for g in range(len(tries)):
-            if tries[g] == code[i] and g!=i:
-                Y +=1
+    while(tries < maxTries):
+        comb = []
+                #to try and guess 5 colors, just add a fifth
+        first_l = input("Write your first letter :  ")
+        comb.append(first_l)
+                     
+        second_l = input("Write your second letter :  ")
+        comb.append(second_l)
+                     
+        third_l = input("Write your third letter :  ")
+        comb.append(third_l)
+                     
+        fourth_l = input("Write your fourth letter :  ")
+        comb.append(fourth_l)
+        
+        print(comb)
+        for i in range(len(code_test)):
+            if comb[i] == code_test[i]:
+                X +=1
+        for i in range(len(code_test)):
+            for g in range(len(comb)):
+                if comb[g] == code_test[i] and g!=i:
+                    code_test[i] = 0
+                    Y +=1
+        tries += 1
+        triesLeft = maxTries - tries
+        print(str(triesLeft) + " tries left")
+        print ("X = " + str(X))
+        print ("Y = " + str(Y))
+        X = 0 
+        Y = 0 
     play = False
-
-print (X,Y)
