c82ecc87a4c3dab498767bd7451481b9cfe614b9
AlbertSMEETS <albert.smeets@laposte.net>
2023-09-14 09:52:42 +0200

    End order n°3
    Adding the ability to play again, reset stats or stop playing. The game will also tell you your stats every time you do something like playing again, reset stats or start playing.

--- main.py
+++ main.py
@@ -1,62 +1,110 @@
 import random
 import string
 
-play = True
-tries = 0
-maxTries = 2 #Max tries 
 
-X = 0  #right color, right place
-Y = 0  #right color, wrong place
-
-colors = ['R','G','B','Y','P','W']
-
-"""4 is the number of color we have to guess, to change this number,
-you also have to add an Fith in the while, or del the fourth"""
-code = random.sample(colors,4)
-
-print(code)
-print("Colors are Red : R, Green : G, Blue : B, Yellow : Y, Purple : P and White : W.\n")
-
-while play:
+def main():
+    play = True
+    tries = 0
+    maxTries = 2 #Max tries 
+    
+    X = 0  #right color, right place
+    Y = 0  #right color, wrong place
+    
+    colors = ['R','G','B','Y','P','W']
+        
+    file = open(".score.txt", "r")
+    data = file.readlines()
+    score = int(data[0].strip("\n"))
+    file.close()
+                
+    file = open(".parties.txt", "r")
+    data = file.readlines()
+    nb = int(data[0].strip("\n")) 
+    file.close()  
+    
+    """4 is the number of color we have to guess, to change this number,
+    you also have to add an Fith in the while, or del the fourth"""
     code = random.sample(colors,4)
-    while(tries < maxTries):
-        code_test = code.copy()#copying the code to be able to change it without killing the real one later
-        comb = []
-                #to try and guess 5 colors, just add a fifth
-        first_l = input("Write your first letter :  ")
-        comb.append(first_l)
-                     
-        second_l = input("Write your second letter :  ")
-        comb.append(second_l)
-                     
-        third_l = input("Write your third letter :  ")
-        comb.append(third_l)
-                     
-        fourth_l = input("Write your fourth letter :  ")
-        comb.append(fourth_l)
-        comb_test = comb.copy()
-        
-        print(comb)
-        for i in range(len(code_test)):
-            if comb_test[i] == code_test[i]:
-                X +=1
-        for i in range(len(code_test)):
-            for g in range(len(comb_test)):
-                if comb_test[g] == code_test[i] and g!=i:
-                    comb_test[g] = 1          #changing the letters to 1 already compared to avoid duplicates in Y
-                    code_test[i] = 0          #changing the letters to 0 already compared to avoid duplicates in Y
-                    Y +=1
-        tries += 1
-        triesLeft = maxTries - tries
-        print(str(triesLeft) + " tries left")
-        print ("X = " + str(X))
-        print ("Y = " + str(Y))
-        X = 0 
-        Y = 0
-        
-    answer = input("Voulez vous rejouer, reinitialiser les stats ou quitter ? 1/2/3\n")
-        
-    if(answer == "1"):
+    
+    
+    print(code)
+    print("Colors are Red : R, Green : G, Blue : B, Yellow : Y, Purple : P and White : W.\n")
+    
+    while play:
         tries = 0
-    elif(answer == "3"):
-        play = False
+        print("Your score is " + str(score))
+        print("You have played  " + str(nb) + " times")
+        while(tries < maxTries):
+            code_test = code.copy()#copying the code to be able to change it without killing the real one later
+            comb = []
+                    #to try and guess 5 colors, just add a fifth
+            first_l = input("Write your first letter :  ")
+            comb.append(first_l)
+                         
+            second_l = input("Write your second letter :  ")
+            comb.append(second_l)
+                         
+            third_l = input("Write your third letter :  ")
+            comb.append(third_l)
+                         
+            fourth_l = input("Write your fourth letter :  ")
+            comb.append(fourth_l)
+            comb_test = comb.copy()
+            
+            print(comb)
+            for i in range(len(code_test)):
+                if comb_test[i] == code_test[i]:
+                    X +=1
+            for i in range(len(code_test)):
+                for g in range(len(comb_test)):
+                    if comb_test[g] == code_test[i] and g!=i:
+                        comb_test[g] = 1          #changing the letters to 1 already compared to avoid duplicates in Y
+                        code_test[i] = 0          #changing the letters to 0 already compared to avoid duplicates in Y
+                        Y +=1
+            tries += 1
+            triesLeft = maxTries - tries
+            print(str(triesLeft) + " tries left")
+            print ("X = " + str(X))
+            print ("Y = " + str(Y))
+            X = 0 
+            Y = 0
+        if tries == maxTries:
+            file = open(".score.txt", "r")
+            data = file.readlines()
+            score = int(data[0].strip("\n")) + tries
+            file.close()
+                
+            file = open(".score.txt", "w")
+            file.write(str(score))
+            file.close()
+                
+            file = open(".parties.txt", "r")
+            data = file.readlines()
+            nb = int(data[0].strip("\n")) + 1
+            file.close()
+                
+            file = open(".parties.txt", "w")
+            file.write(str(nb))
+            file.close()
+            
+        print("Your score is " + str(score))
+        print("You have played  " + str(nb) + " times")
+        answer = input("Do you want to play again, rest stats or quit ? 1/2/3\n")
+            
+        if(answer == "1"):  
+            tries = 0           #Playing again
+        elif(answer == "2"):
+            score = 0                     #Reset scores and parties
+            nb = 0
+            file = open(".score.txt", "w")
+            file.write(str(0))
+            print("Score has been reset")
+            file.close()
+            
+            file = open(".parties.txt", "w")
+            file.write(str(0))
+            print("Parties has been reset")
+            file.close()
+        elif(answer == "3"):
+            play = False        #Stopping the game
+main()
